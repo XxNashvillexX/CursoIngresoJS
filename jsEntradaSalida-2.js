c57e963cb88f4887e25d34526ccf5346ef9f6da4
XxNashvillexX <36193756+XxNashvillexX@users.noreply.github.com>
2018-02-06 10:15:57 -0300

    Ejercicio 1 y 2
    PromPT

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,20 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var nombre;
+	//esto es un comentario de una linea
+
+	//alert(nombre);
+	/*
+	esto es un bloque
+	alert("nombre");
+	nombre="Matías";
+	alert(nombre);
+	*/
+	nombre=prompt("ingrese nombre","natalia natalia");
+	alert("su nombre es: "+nombre);
+
+	//var person = prompt("Please enter your name", "Harry Potter");
+}
+
