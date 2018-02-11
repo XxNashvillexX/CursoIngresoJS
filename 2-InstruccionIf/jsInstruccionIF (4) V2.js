4e564f0376129a9d54f7dd6a299f2e228352da7d
XxNashvillexX <36193756+XxNashvillexX@users.noreply.github.com>
2018-02-10 23:38:22 -0300

    ejercicio if 4
    Metodo N°2: con "else"

---
+++ 2-InstruccionIf/jsInstruccionIF (4) V2.js
@@ -0,0 +1,26 @@
+function Mostrar()
+{
+//tomo la edad  
+
+	var edad;
+	edad=document.getElementById('edad').value;
+
+		if(edad>=11&&edad<=18)
+		{
+			alert("adolescente")
+		}
+			
+			else
+				{
+					alert("no adolescente");
+				}
+
+
+				/*
+				Operadores logicos:
+				&&="y"
+				||="o"
+				!="negacion"
+				*/
+				
+}//FIN DE LA FUNCIÓN
