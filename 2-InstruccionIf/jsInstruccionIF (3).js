ca06881e1809f5669a3b0e0fdb1ff74053aa4f15
XxNashvillexX <36193756+XxNashvillexX@users.noreply.github.com>
2018-02-10 23:08:10 -0300

    ejercicio if 3
    Metodo N°1: incluyendo "else"

---
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -0,0 +1,20 @@
+function Mostrar()
+{
+//tomo la edad  
+
+	var edad;
+	edad=document.getElementById('edad').value;
+
+		if(edad<=17)
+
+			{
+				alert("menor de edad");
+			}
+
+		if(edad>17)
+
+			{
+				alert("mayor de edad");
+			}
+	
+}//FIN DE LA FUNCIÓN
