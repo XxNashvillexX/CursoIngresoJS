d76dbb31d9a4841ef8efe03d0e62370b0d81170b
XxNashvillexX <36193756+XxNashvillexX@users.noreply.github.com>
2018-02-11 00:17:22 -0300

    ejercicio if 5
    Método N°3: solo uso de "if"

---
+++ 2-InstruccionIf/jsInstruccionIF (5) V3.js
@@ -0,0 +1,19 @@
+function Mostrar()
+{
+//tomo la edad  
+
+	var edad;
+	edad=document.getElementById('edad').value;
+
+		if(edad<13)
+
+		{
+			alert("No adolescente");
+		}
+		
+		if(edad>17)
+			
+		{
+			alert("Adolescente");
+		}
+}//FIN DE LA FUNCIÓN
