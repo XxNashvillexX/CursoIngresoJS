5dedff4d71fcc5f6ffdd6442c710a6f2707d0ac3
XxNashvillexX <36193756+XxNashvillexX@users.noreply.github.com>
2018-02-11 00:00:35 -0300

    ejercicio if 5
    Método N°1: sin "else" y con operador lógico "||"

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -2,5 +2,12 @@ function Mostrar()
 {
 //tomo la edad  
 
+	var edad;
+	edad=document.getElementById('edad').value;
 
+		if(edad<13||edad>18) 
+			
+			{
+				alert("No adolescente");
+			}
 }//FIN DE LA FUNCIÓN
