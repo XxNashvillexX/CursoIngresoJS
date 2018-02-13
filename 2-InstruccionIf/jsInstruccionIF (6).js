c4b1c7eadfd77037a20064cfb403626098a208c4
XxNashvillexX <36193756+XxNashvillexX@users.noreply.github.com>
2018-02-12 23:26:29 -0300

    ejercicio if 6
    Método N°1: sin "else"

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,23 @@ function Mostrar()
 {
 //tomo la edad  
 
+	var edad;
+	edad=document.getElementById('edad').value;
 
+		if(edad<=12)
+		
+		{
+			alert("Niño");
+		}
 
+		if(edad>=13&&edad<=17)
+		
+		{
+			alert("Adolescente");
+		}
 
+		if(edad>=18)
+		{
+			alert("Mayor de edad");
+		}
 }//FIN DE LA FUNCIÓN
