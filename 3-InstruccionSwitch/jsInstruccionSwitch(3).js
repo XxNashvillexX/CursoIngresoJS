15ee260b697a4c8cf3e6b06b3b6078c896c6a0b8
XxNashvillexX <36193756+XxNashvillexX@users.noreply.github.com>
2018-02-14 12:24:01 -0300

    ejercicio Switch 1,2 y 3

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,34 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
+	switch (mesDelAño)
+	{
+		case "Enero";
 
+		case "Marzo";
 
+		case "Abril";
+
+		case "Mayo";
+
+		case "Junio";
+
+		case "Agosto";
+
+		case "Septiembre";
+
+		case "Octubre";
+
+		case "Noviembre";
+
+		case "Diciembre";
+			alert("Este mes tiene 30 o más días");
+			break;
+
+		case "febrero":
+			alert("Este mes no tiene más de 29 días");
+			break;
+	}
 }//FIN DE LA FUNCIÓN
