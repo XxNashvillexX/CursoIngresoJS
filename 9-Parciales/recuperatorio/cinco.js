5768a526a3fffb7318b4f2d61c45faf24ad52121
XxNashvillexX <36193756+XxNashvillexX@users.noreply.github.com>
2018-02-18 12:44:50 -0300

    ejercicio 4 y 5 parcial 2

--- 9-Parciales/recuperatorio/cinco.js
+++ 9-Parciales/recuperatorio/cinco.js
@@ -1,4 +1,40 @@
 function Mostrar()
 {
 
+	var	mes;
+	mes=prompt("Ingrese mes");
+
+	switch(mes)
+	{
+		case "enero":
+			alert("Comienza el año")
+			break;
+
+		case "febrero":
+
+		case "marzo":
+
+		case "abril":
+
+		case "mayo":
+		
+		case "junio":
+
+		case "julio":
+
+		case "agosto":
+
+		case "septiembre":
+
+		case "octubre":
+
+		case "noviembre":
+			alert("No es enero ni diciembre")
+			break;
+
+		case "diciembre":
+			alert("Se vienen las fiestas")
+			break;
+	}
+
 }
--- 9-Parciales/recuperatorio/cuatro.js
+++ 9-Parciales/recuperatorio/cuatro.js
@@ -1,4 +1,41 @@
 function Mostrar()
 {
+	var text1;
+	var text2;
+	var	text3;
+	var	numeroUno;
+	var numeroDos;
+	var resultado;
 
+	text1=("Negativo");
+	text2=("Positivo");
+	text3=("cero");
+	numeroUno=prompt("ingrese primer numero");
+	numeroUno=parseInt(numeroUno);
+	numeroDos=prompt("ingrese segundo numero");
+	numeroDos=parseInt(numeroUno);
+
+	resultado=numeroUno+numeroDos;
+
+		if (resultado <= -1)
+		
+		{
+			document.write(text1)
+		}
+
+		else
+
+		{
+			if (resultado >= 1)
+			
+			{
+				document.write(text2)
+			}
+
+			else
+
+			{
+				document.write(text3)
+			}
+		}
 }
