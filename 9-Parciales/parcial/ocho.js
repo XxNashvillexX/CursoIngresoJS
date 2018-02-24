7eac1c031c0077415a947e04ae3e6296823734f0
XxNashvillexX <36193756+XxNashvillexX@users.noreply.github.com>
2018-02-24 10:55:50 -0300

    Add files via upload

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -1,4 +1,19 @@
+/*ingreso de alumnos, no se sabe cuantos hay. Se pide nombre, cuantos alumnos se ingreso.
+con document.write mostrar la cantidad de alumnos ingresados.*/
 function Mostrar()
+
 {
 
+	var alumno;
+	var respuesta;
+	var	cantDeAlumnos;
+
+		while(alumno != "no")
+		{
+			alumno=prompt("ingrese nombre de alumno");
+			cantDeAlumnos=cantDeAlumnos+1;
+			alumno=prompt("¿desea ingresar otro alumno?")
+		}
+
+		document.write("la cantidad de alumnos es: " + cantDeAlumnos);
 }
